d52c224698ae0a1c6f09ef2680e0bad8fff5e20b
karthikeya <94825806+Karthik2830@users.noreply.github.com>
2022-03-16 21:35:50 +0530

    Update and rename README.md to ncr.sql

---
+++ ncr.sql
@@ -0,0 +1,37 @@
+# sql-plus
+1.	Write a PL/SQL stored function to calculate ncr using fact (m) function that returns factorial of m. 
+
+PROGRAM 1:
+
+FUNCTION FOR CALCULATING FACTORIAL
+
+CREATE or replace FUNCTION fact (n number) RETURN number AS
+f  number := 1;
+a  number := n;
+BEGIN
+WHILE (a > 1 ) LOOP
+f := f * a;
+a := a - 1;
+END LOOP;
+RETURN f;
+END;
+
+FUNCTION FOR CALCULATING NCR
+
+CREATE or replace FUNCTION ncr (n number, r number) RETURN number AS
+ncr  number ;
+BEGIN
+ncr:= trunc(fact(n)/(fact(r)*fact(n-r)));
+RETURN ncr;
+END;
+
+MAIN PROGRAM
+
+declare
+n number := &n;
+r number := &r;
+ncr1 number;
+begin
+ncr1 := ncr(n,r);
+dbms_output.put_line(n||' c '||r||' = '||ncr1);
+end;
